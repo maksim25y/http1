175ab5a81b272f21e7489d07fcce961df364807b
mudan <myudanov223@gmail.com>
2024-01-03 23:26:10 +0300

    create dao, dto, servlet flight, service flight

---
+++ src/service/FlightService.java
@@ -0,0 +1,34 @@
+package service;
+
+
+import dao.FlightDao;
+import dto.FlightDto;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class FlightService {
+
+    private static final FlightService INSTANCE = new FlightService();
+
+    private final FlightDao flightDao = FlightDao.getInstance();
+
+    private FlightService() {
+    }
+
+    public List<FlightDto> findAll() {
+        return flightDao.findAll().stream()
+                .map(flight -> new FlightDto(
+                        flight.getId(),
+                        """
+                            %s - %s - %s
+                        """.formatted(flight.getDepartureAirportCode(), flight.getArrivalAirportCode(), flight.getStatus())
+                ))
+                .collect(toList());
+    }
+
+    public static FlightService getInstance() {
+        return INSTANCE;
+    }
+}
