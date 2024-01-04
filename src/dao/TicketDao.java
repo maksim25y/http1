633ea6d53545bc307759a0d3c7e8e11e10939626
mudan <myudanov223@gmail.com>
2024-01-04 15:05:46 +0300

    create ticket: entity, dao, dto, service, servlet

--- src/dao/TicketDao.java
+++ src/dao/TicketDao.java
@@ -11,20 +11,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class TicketDao implements Dao<Long, Ticket>{
-    private static final TicketDao INSTANCE = new TicketDao();
-    private static final String FIND_ALL_BY_FLIGHT_ID = "SELECT * FROM ticket";
+public class TicketDao implements Dao<Long, Ticket> {
 
-    public TicketDao() {
+    private static final TicketDao INSTANCE = new TicketDao();
+
+    public static final String FIND_ALL_BY_FLIGHT_ID = """
+            SELECT *
+            FROM ticket
+            WHERE flight_id = ?
+            """;
+
+    private TicketDao() {
     }
-    public static TicketDao getInstance(){
+
+    public static TicketDao getInstance() {
         return INSTANCE;
     }
 
-    @Override
-    public List<Ticket> findAll() {
-        return null;
-    }
     public List<Ticket> findAllByFlightId(Long flightId) {
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(FIND_ALL_BY_FLIGHT_ID)) {
@@ -35,6 +38,7 @@ public class TicketDao implements Dao<Long, Ticket>{
             while (resultSet.next()) {
                 tickets.add(buildTicket(resultSet));
             }
+
             return tickets;
         } catch (SQLException throwables) {
             throw new RuntimeException(throwables);
@@ -42,12 +46,19 @@ public class TicketDao implements Dao<Long, Ticket>{
     }
 
     private Ticket buildTicket(ResultSet resultSet) throws SQLException {
-        return new Ticket(resultSet.getObject("id",Long.class),
-                resultSet.getObject("passenger_no",String.class),
-                resultSet.getObject("passenger_name",String.class),
-                resultSet.getObject("flight_id",Long.class),
-                resultSet.getObject("seat_no",String.class),
-                resultSet.getObject("cost", BigDecimal.class));
+        return new Ticket(
+                resultSet.getObject("id", Long.class),
+                resultSet.getObject("passenger_no", String.class),
+                resultSet.getObject("passenger_name", String.class),
+                resultSet.getObject("flight_id", Long.class),
+                resultSet.getObject("seat_no", String.class),
+                resultSet.getObject("cost", BigDecimal.class)
+        );
+    }
+
+    @Override
+    public List<Ticket> findAll() {
+        return null;
     }
 
     @Override
