cae5375d44e34d0953df15e167624df4c1917174
mudan <myudanov223@gmail.com>
2024-01-04 18:26:38 +0300

    create session and get attributes

---
+++ src/servlet/SessionServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import dto.UserDto;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/sessions")
+public class SessionServlet extends HttpServlet {
+    private final String USER = "user";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var session = req.getSession();
+        var user = (UserDto)session.getAttribute(USER);
+        if(user==null){
+            user = UserDto.builder().id(25L).mail("test@gmail.com").build();
+            session.setAttribute(USER,user);
+        }
+        System.out.println(session.isNew());
+    }
+}
